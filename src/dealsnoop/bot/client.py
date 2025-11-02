496cdcd6d266008ffd4b3d0790019ee729a54eba
iRedSC <59147735+iRedSC@users.noreply.github.com>
2025-11-02 17:51:13 -0500

    Fixed async issue with registering cogs

--- src/dealsnoop/bot/client.py
+++ src/dealsnoop/bot/client.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import asyncio
 from discord.ext import commands
 import discord
 from dealsnoop.logger import logger
@@ -17,28 +18,29 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 class Client(commands.Bot):
-    
+    _unregistered_cogs: list[commands.Cog]
 
     def __init__(self):
         super().__init__(command_prefix="!@#", intents=intents)
-        
+        self._unregistered_cogs = []
     
 
-
     def register_cog(self, cog: commands.Cog):
-        logger.info("Registering Commands (sync)")
+        self._unregistered_cogs.append(cog)
 
-        async def _register():
-            await self.add_cog(cog)
-            for command in cog.get_app_commands():
-                self.tree.add_command(command, guild=GUILD_ID)
-                logger.info(f"Added command '{command.name}'")
 
-        self.loop.create_task(_register())
+    async def _register_cog(self, cog: commands.Cog):
+        logger.info("Registering Commands")
+        await self.add_cog(cog)
+        for command in cog.get_app_commands():
+            self.tree.add_command(command, guild=GUILD_ID)
+            logger.info(f"Added command '{command.name}'")
 
 
 
     async def setup_hook(self) -> None:
+        for cog in self._unregistered_cogs:
+            await self._register_cog(cog)
         await self.tree.sync(guild=GUILD_ID)
         logger.info("$G$Command tree synced")
 
